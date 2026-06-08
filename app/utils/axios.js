62dce244435b102c6bd27a53f2f9c4431c290010
Ashik Ahmed <md.ashikahmed@outlook.com>
2026-06-08 14:30:29 +0600

    api url fix

--- app/utils/axios.js
+++ app/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://api.buyzin.com', // http://127.0.0.1:8000 http://api.buyzin.com
+  baseURL: 'https://api.buyzin.com', // http://127.0.0.1:8000 https://api.buyzin.com
   headers: {
     "Content-type": "application/json",
     Author: "Ashik Ahmed",
