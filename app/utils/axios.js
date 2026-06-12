007756cca298f0f05f5820a9502390d8dab866c8
Ashik Ahmed <md.ashikahmed@outlook.com>
2026-06-12 20:33:08 +0600

    app signature added

--- app/utils/axios.js
+++ app/utils/axios.js
@@ -9,7 +9,7 @@ const apiClient = axios.create({
         "Content-Type": "application/json",
         Accept: "application/json",
         "X-Source": "Web",
-        "X-APP-KEY": "base64:DZ/DIBMHkGIXA42v3OjTmcJa+L3VWvQ44lbq4LVVAjA=",
+        "X-APP-KEY": "$2y$10$.RUlpmXqOBSnmxl67aHDC.uowDpsgkIjGn/cU9iFXKmbnlkQMWfG2",
     },
     withCredentials: false,
 });
