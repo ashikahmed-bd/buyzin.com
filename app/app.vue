12c182c4472fa1fbc8e6cd0dc0debe9063313030
Ashik Ahmed <md.ashikahmed@outlook.com>
2026-06-13 12:10:37 +0600

    og image added

--- app/app.vue
+++ app/app.vue
@@ -12,6 +12,10 @@ useSchemaOrg([
     description: "Buyzin is Bangladesh’s trusted ecommerce platform for fashion, electronics, gadgets, lifestyle products, and everyday essentials.",
     url: "https://buyzin.com",
     logo: "https://buyzin.com/logo.svg",
+    image: [
+      "https://buyzin.com/logo.svg",
+      "https://buyzin.com/og-image.jpg",
+    ],
     email: "support@buyzin.com",
     telephone: "+880-9649-492233",
     sameAs: [
@@ -22,15 +26,27 @@ useSchemaOrg([
       "https://www.pinterest.com/BuyzinOfficial",
       "https://www.linkedin.com/company/buyzinofficial",
     ],
+
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "Rowmari",
+      addressLocality: "Kurigram",
+      addressRegion: "Rangpur Division",
+      postalCode: "5640",
+      addressCountry: "Bangladesh",
+    },
+
     contactPoint: [
       {
         "@type": "ContactPoint",
         telephone: "+880-1911-742233",
         contactType: "customer support",
-        areaServed: "BD",
+        areaServed: "Bangladesh",
         availableLanguage: ["English", "Bengali"],
       },
     ],
+
+    foundingDate: "2025",
   }),
 ]);
 </script>
